24bdc23996e948d59ae797b2eace54d29b6b7361
Rishabh <rawatrishi1121@gmail.com>
2025-10-08 21:25:48 +0530

    understood walrus operator

---
+++ python (day 3)/first.py
@@ -0,0 +1,22 @@
+"""data = input("Enter some data ")
+while data!='quit':
+    print("Entered data ", data)
+    data=input("Enter some data")
+"""
+# 
+
+
+# while data = input("Enter some data ")!='quit':
+#     print("Entered data ", data)
+#     data=input("Enter some data")
+
+while (data := input("Enter some data "))!='quit':
+    print("Entered data ", data)
+    # data=input("Enter some data")
+
+"""
+
+Enter some data tuesday
+Entered data  True
+
+"""
---
+++ python (day 3)/readme.md
@@ -0,0 +1,7 @@
+### Day 3 : Python Walrus operator.
+
+(:=) at same time it will assign value and compare with the value.
+
+
+Enter some data tuesday
+Entered data  True
